8d218bce5327d3de7d9d089d0b63be21e1398c7a
Roi <thedarkhermit@gmail.com>
2021-05-12 19:59:30 +0300

    before perceptron

--- HW2_ID1_ID2.py
+++ HW2_ID1_ID2.py
@@ -13,7 +13,9 @@ class PerceptronClassifier:
         Constructor for the PerceptronClassifier.
         """
         # TODO - Place your student IDs here. Single submitters please use a tuple like so: self.ids = (123456789,)
-        self.ids = (123456789, 987654321)
+        self.ids = (316327451, 206230021)
+        self.W = 0
+        self.number_of_clases = 0;
 
     def fit(self, X: np.ndarray, y: np.ndarray):
         """
@@ -23,9 +25,30 @@ class PerceptronClassifier:
         :param y: A 1-dimensional numpy array of m rows. it is guaranteed to match X's rows in length (|m_x| == |m_y|).
         Array datatype is guaranteed to be np.uint8.
         """
+        # rand = np.random.RandomState(self.random_state)
+        ones = np.ones((X.shape[0], 1))
+        X = np.c_[X, ones]
+        self.numer_of_classes = len(np.unique(y, return_counts=False))
+        Wmat = np.zeros((self.numer_of_classes, X.shape[1] - 1))
+        ones = np.ones((self.numer_of_classes, 1))
+        self.W = np.c_[Wmat, ones]
+        convergence = 0
+        while convergence == 0:
+            convergence = 1
+            for j, example in enumerate(X):
+                maxArg = [0, 0]
+                for i, label in enumerate(np.unique(y, return_counts=False)):
+                    curr_y = np.inner(example, self.W[i])
+                    if curr_y > maxArg[0]:
+                        maxArg[0] = curr_y
+                        maxArg[1] = label
+                if maxArg[1] != y[j]:
+                    self.W[y[j]] = self.W[y[j]] + example
+                    self.W[maxArg[1]] = self.W[maxArg[1]] - example
+                    convergence = 0
+        # print(self.W)
 
-        # TODO - your code here
-        pass
+    # self.W = np.zeros(X.shape[1])
 
     def predict(self, X: np.ndarray) -> np.ndarray:
         """
@@ -35,16 +58,24 @@ class PerceptronClassifier:
         Array datatype is guaranteed to be np.float32.
         :return: A 1-dimensional numpy array of m rows. Should be of datatype np.uint8.
         """
-
-        # TODO - your code here
-        pass
+        ones = np.ones((X.shape[0], 1))
+        X = np.c_[X, ones]
+        Prediction = []
+        for j, example in enumerate(X):
+            maxArg = [0, 0]
+            for i in range(self.numer_of_classes):
+                curr_y = np.dot(example, self.W[i])
+                if curr_y > maxArg[0]:
+                    maxArg[0] = curr_y
+                    maxArg[1] = i
+            Prediction.append(maxArg[1])
+        return np.array(Prediction)
 
         ### Example code - don't use this:
         # return np.random.randint(low=0, high=2, size=len(X), dtype=np.uint8)
 
 
 if __name__ == "__main__":
-
     print("*" * 20)
     print("Started HW2_ID1_ID2.py")
     # Parsing script arguments
@@ -71,5 +102,4 @@ if __name__ == "__main__":
     print("Done")
     accuracy = np.sum(y_pred == y.ravel()) / y.shape[0]
     print(f"Train accuracy: {accuracy * 100 :.2f}%")
-
     print("*" * 20)
--- main.py
+++ main.py
@@ -73,20 +73,81 @@ def main():
     Y_val = val_df["target"]
     set1 = [X, Y]
     set2 = [X_val, Y_val]
-    margins=[]
+    margins = []
+    test_error = []
+    train_error = []
     for c in [0.01, 0.05, 0.1]:
-        for set in [set1, set2]:
-            model = SVC(kernel='linear', C=c)
-            model.fit(set[0], set[1])
-            plt.scatter(set[0][:, 0], set[0][:, 1], c=set[1], s=50, cmap='autumn')
-            plot_svc_decision_function(model)
-            plt.title('scatter with C=' + str(c))
-            plt.show()
-            margins.append(1 / (np.sqrt(np.sum(model.coef_ ** 2))))
-            accuracy_score()
+        model = SVC(kernel='linear', C=c)
+        model.fit(set1[0], set1[1])
+        plt.scatter(set1[0][:, 0], set1[0][:, 1], c=set1[1], s=50, cmap='autumn')
+        plot_svc_decision_function(model)
+        plt.title('Train scatter with C =' + str(c))
+        plt.show()
+        margins.append(1 / (np.sqrt(np.sum(model.coef_ ** 2))))
+        test_error.append(1 - model.score(X_val, Y_val))
+        train_error.append(1 - model.score(X, Y))
+    for c in [0.01, 0.05, 0.1]:
+        model = SVC(kernel='linear', C=c)
+        model.fit(set1[0], set1[1])
+        plt.scatter(set2[0][:, 0], set2[0][:, 1], c=set2[1], s=50, cmap='autumn')
+        plot_svc_decision_function(model, plot_support=False)
+        plt.title('Test scatter with C=' + str(c))
+        plt.show()
 
+    plt.plot([0.01, 0.05, 0.1], margins[0:3])
+    plt.title('train: margin vs c')
+    plt.ylabel("margin")
+    plt.xlabel("C")
+    plt.show()
 
+    # ********* 1.4
+
+    plt.plot([0.01, 0.05, 0.1], test_error)
+    plt.plot([0.01, 0.05, 0.1], train_error)
+    plt.title("Train Vs Validation")
+    plt.ylabel('error rate')
+    plt.xlabel('C')
+    plt.legend(['validation', 'train'])
+    plt.show()
     # *********
+    # 1.6
+    train_error = []
+    test_error = []
+    for deg in [2, 3, 4, 5, 6, 7, 8]:
+        modelNew = SVC(kernel='poly', degree=deg, C=1)
+        modelNew.fit(set1[0], set1[1])
+        train_error.append(1 - modelNew.score(X, Y))
+        test_error.append(1 - modelNew.score(X_val, Y_val))
+    plt.plot([2, 3, 4, 5, 6, 7, 8], train_error)
+    plt.plot([2, 3, 4, 5, 6, 7, 8], test_error)
+    plt.title("dimension comparison")
+    plt.ylabel('error rate')
+    plt.xlabel('dimension')
+    plt.legend(['train', 'validation'])
+    plt.show()
+
+    # ********** 1.7
+
+    for d in [2,5]:
+        model = SVC(kernel='poly', C=1, degree=d)
+        model.fit(set1[0], set1[1])
+        plt.scatter(set1[0][:, 0], set1[0][:, 1], c=set1[1], s=50)
+        plot_svc_decision_function(model)
+        plt.title('Train scatter with Degree =' + str(d))
+        plt.show()
+        # margins.append(1 / (np.sqrt(np.sum(model.coef_ ** 2))))
+        test_error.append(1 - model.score(X_val, Y_val))
+        train_error.append(1 - model.score(X, Y))
+    for d in [2, 5]:
+        model = SVC(kernel='poly', C=1, degree=d)
+        model.fit(set1[0], set1[1])
+        plt.scatter(set2[0][:, 0], set2[0][:, 1], c=set2[1], s=50 )
+        plot_svc_decision_function(model, plot_support=False)
+        plt.title('Test scatter with Degree=' + str(d))
+        plt.show()
+
+
+
 
 if __name__ == "__main__":
     main()
